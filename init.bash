74a04dd0d03b85febd730201ed6c1d6a2c7f0dc9
Frank X. Blackburn <6500159+axmmisaka@users.noreply.github.com>
2022-11-20 00:56:14 -0800

    Create init.bash

---
+++ init.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update
+npm install -g typescript
+sudo apt install rustc
+git clone https://github.com/lf-lang/lingua-franca.git --branch master --depth 1
+cd lingua-franca
+./gradlew buildAll
+cd ..
+git clone https://github.com/lf-lang/examples-lingua-franca.git examples --branch main
